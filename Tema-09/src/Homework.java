7704e5d708547fd4e406fcb6e7d4e839f2a7f762
ZDRAVKO107UKTC <146438118+ZDRAVKO107UKTC@users.noreply.github.com>
2025-01-18 22:16:57 +0200

    Buble sort implementation

--- Tema-09/src/Homework.java
+++ Tema-09/src/Homework.java
@@ -1,47 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
-import java.util.Scanner;
 
 public class Homework {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter the number of elements in the array: ");
-
-        int n = sc.nextInt();
+        int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
         Random rand = new Random();
 
         for (int i = 0; i < n; i++) {
-
             arr[i] = rand.nextInt(100) + 1;
-
         }
 
-        System.out.println("\nArray:");
-        printArray(arr);
+        System.out.println("\nArray: " + Arrays.toString(arr));
 
         List<Integer> evenElements = new ArrayList<>();
         List<Integer> oddElements = new ArrayList<>();
 
         separateEvenOdd(arr, evenElements, oddElements);
 
-        System.out.println("\nEven el:");
-        printList(evenElements);
-        System.out.println("\nOdd el:");
-        printList(oddElements);
+        System.out.println("\nEven el: " + Arrays.toString(evenElements.toArray()));
+        System.out.println("\nOdd el: " + Arrays.toString(oddElements.toArray()));
 
         bubbleSortAscending(evenElements);
         bubbleSortDescending(oddElements);
 
-        System.out.println("\nEven el ascending:");
-        printList(evenElements);
-        System.out.println("\nOdd el descending:");
-        printList(oddElements);
+        System.out.println("\nEven el ascending: " + Arrays.toString(evenElements.toArray()));
+        System.out.println("\nOdd el descending: " + Arrays.toString(oddElements.toArray()));
+
     }
-
-
-
 
     public static void separateEvenOdd(int[] arr, List<Integer> evenElements, List<Integer> oddElements) {
         for (int num : arr) {
@@ -53,13 +46,11 @@ public class Homework {
         }
     }
 
-
     public static void bubbleSortAscending(List<Integer> arr) {
         int n = arr.size();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr.get(j) > arr.get(j + 1)) {
-
                     int temp = arr.get(j);
                     arr.set(j, arr.get(j + 1));
                     arr.set(j + 1, temp);
@@ -73,7 +64,6 @@ public class Homework {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr.get(j) < arr.get(j + 1)) {
-
                     int temp = arr.get(j);
                     arr.set(j, arr.get(j + 1));
                     arr.set(j + 1, temp);
@@ -82,17 +72,4 @@ public class Homework {
         }
     }
 
-    public static void printArray(int[] arr) {
-        for (int num : arr) {
-            System.out.print(num + " ");
-        }
-        System.out.println();
-    }
-
-    public static void printList(List<Integer> list) {
-        for (int num : list) {
-            System.out.print(num + " ");
-        }
-        System.out.println();
-    }
 }
