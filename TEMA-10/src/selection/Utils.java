f6488bc8db83c9f329e3290ff69b24404a17d2d9
ZDRAVKO107UKTC <146438118+ZDRAVKO107UKTC@users.noreply.github.com>
2025-01-29 12:31:56 +0200

    homework

---
+++ TEMA-10/src/selection/Utils.java
@@ -0,0 +1,15 @@
+package selection;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+public class Utils {
+    public static void inputs(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader());
+
+
+
+
+    }
+}
